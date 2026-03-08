07328b0e5b076de66306884b5930b21a90c2e3bc
Junaed242 <abdullah56429@gmail.com>
2026-03-09 02:11:14 +0600

    Cards loaded

---
+++ main.js
@@ -0,0 +1,110 @@
+let allIssues = []; // Global store for filtering
+
+// 1. Fetch all issues from API
+const loadInitialData = async () => {
+  const grid = document.getElementById("issues-grid");
+  const spinner = document.getElementById("loading-spinner");
+
+  // Show Loading Spinner
+  spinner.classList.remove("hidden");
+
+  try {
+    const res = await fetch(
+      "https://phi-lab-server.vercel.app/api/v1/lab/issues",
+    );
+    const result = await res.json();
+    allIssues = result.data; // Store data for tab filtering
+    displayIssues(allIssues);
+  } finally {
+    spinner.classList.add("hidden");
+  }
+};
+
+// 2.Grid Generator
+const displayIssues = (issues) => {
+  const grid = document.getElementById("issues-grid");
+  const countElement = document.getElementById("issue-count");
+
+  grid.innerHTML = "";
+  countElement.innerText = issues.length; // Update  count
+
+  issues.forEach((issue) => {
+    const card = document.createElement("div");
+
+    //styling based on JSON status
+    const isOpen = issue.status === "open";
+    const statusColor = isOpen
+      ? "./assets/Open-Status.png"
+      : "./assets/Closed-Status.png";
+    const borderColor = isOpen ? "border-emerald-500" : "border-[#631dfa]";
+
+    card.className = `card bg-white shadow-sm border  border-t-4 ${borderColor} p-5 space-y-4 hover:shadow-md transition-shadow cursor-pointer`;
+    card.onclick = () => loadSingleIssue(issue.id);
+
+    // Cards
+    const labelHtml = issue.labels
+      .map((label) => {
+        const lowerLabel = label.toLowerCase();
+
+        // Default styling
+        let icon = "fa-tag";
+        let colors = "bg-emerald-50 text-emerald-500 border-emerald-100";
+
+        //tag styling
+        if (lowerLabel === "bug") {
+          icon = "fa-bug";
+          colors = "bg-rose-50 text-rose-500 border-rose-100";
+        }
+        else if (lowerLabel === "help wanted") {
+          icon = "fa-circle-dot"; // Or fa-life-ring
+          colors = "bg-amber-50 text-amber-500 border-amber-200";
+        } else if (lowerLabel === "documentation") {
+          icon = "fa-book";
+          colors = "bg-blue-50 text-blue-500 border-blue-100";
+
+        } else if (lowerLabel === "enhancement") {
+          icon = "fa-wand-magic-sparkles";
+          colors = "bg-green-50 text-green-500 border-green-100";
+        }
+
+        return `
+        <span class="flex items-center gap-1.5 px-3 py-1 rounded-full border ${colors} text-[10px] font-bold uppercase tracking-tight">
+            <i class="fa-solid ${icon} text-[9px]"></i> 
+            ${label}
+        </span>
+    `;
+      })
+      .join("");
+
+    card.innerHTML = `
+    <div class="flex justify-between items-center">
+        <div class="flex items-center gap-2">
+            <div class="w-4 h-4 rounded-full border flex items-center justify-center">
+                <img src="${statusColor}" alt="${issue.status}" class="w-4 h-4">
+            </div>
+            <span class="text-[10px] font-bold text-slate-400 uppercase tracking-widest">ISSUE</span>
+        </div>
+        <div class="badge ${issue.priority === "high" ? "badge-error" : issue.priority === "medium" ? "badge-warning" : "badge-ghost"} badge-outline text-[10px] font-extrabold px-3 h-5 uppercase">
+            ${issue.priority}
+        </div>
+    </div>
+
+    <div>
+        <h3 class="font-bold text-slate-800 text-sm mb-2">${issue.title}</h3>
+        <p class="text-slate-400 text-[11px]">${issue.description}</p>
+    </div>
+
+    <div class="flex flex-wrap gap-2">
+        ${labelHtml}
+    </div>
+
+    <div class="border-t border-slate-200 pt-3 flex justify-between items-center text-[10px] text-slate-400 font-medium">
+        <span>#${issue.id} by <span class="text-slate-600 font-bold">${issue.author}</span></span>
+        <span>${new Date(issue.createdAt).toLocaleDateString()}</span>
+    </div>
+`;
+    grid.appendChild(card);
+  });
+};
+
+loadInitialData();
